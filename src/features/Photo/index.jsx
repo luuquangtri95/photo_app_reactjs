2c6aab05305f4780e22ad5dd4f03dfdc574030e8
luuquangtri <luuquangtri95@gmail.com>
2022-01-17 04:58:31 +0700

    pending route

---
+++ src/features/Photo/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+Photo.propTypes = {};
+
+function Photo(props) {
+  return <div></div>;
+}
+
+export default Photo;
---
+++ src/features/Photo/photoSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const photoSlice = createSlice({
+  name: "photo",
+  initialState: [],
+  reducers: {},
+});
+
+const { reducer, actions } = photoSlice;
+// export actions
+
+//export reducer
+export default reducer;
